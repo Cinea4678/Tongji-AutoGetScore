67528137b078bdd191090676500cd8eca093fa9d
Cinea4678 <cinea123@163.com>
2022-08-23 17:17:50 +0800

    setup.py

--- setup.py
+++ setup.py
@@ -7,18 +7,17 @@ Usage:
 
 from setuptools import setup
 
-APP = ['main.py']
-DATA_FILES = ['nmck_bb.ico','tools.py','Ui_checkScore.py','Ui_verifyMail.py']
+APP = ['Tongji-AutoGetScore.py']
+DATA_FILES = ['sources','tools.py','ui']
 OPTIONS = {
     'iconfile':'sources/logo.icns',
     'plist': {
                     'CFBundleName'   : 'Tongji-ScoreAutoGet',     # 应用名
                     'CFBundleDisplayName': '同济成绩自动查询器', # 应用显示名
                     'CFBundleVersion': '0.2.0',      # 应用版本号
-                    'CFBundleIdentifier' : 'tongjiScoreAutoGet', # 应用包名、唯一标识
+                    'CFBundleIdentifier' : 'com.cinea.tjsag', # 应用包名、唯一标识
                     'NSHumanReadableCopyright': 'Copyright © 2022 Cinea Works. Use the GPL license.', # 可读版权
                     'includes': ['loguru', 'PyQt5.QtCore', 'PyQt5.QtWidgets', 'PyQt5.QtGui','requests','PyQt5.QtWebEngineWidgets','PyQt5.QtNetwork']
-                       
            }
 }
 
